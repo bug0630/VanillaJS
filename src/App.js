2e3dcace0a0c10449f60db1e35ff7d6ddf6e96d0
zaya <zayadreambig@gmail.com>
2024-05-01 15:54:33 +0900

    feat: make Page components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router";
+import Main from "./pages/Main";
+import NotFound from "./pages/NotFound";
+import "./styles/reset.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Main />} />
+        <Route path="/artDetail/:artId" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <LoginRegister />
+    </div>
+  );
 }
 
 export default App;
