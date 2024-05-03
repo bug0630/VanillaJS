433fc8b06c7a55a253e27569fb336f6873674935
zaya <zayadreambig@gmail.com>
2024-05-03 16:50:00 +0900

    fix: loginRegister 배경색 지웠음

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Route, Routes } from "react-router";
-import Main from "./pages/Main";
-import NotFound from "./pages/NotFound";
-import "./styles/reset.css";
-import Search from "./pages/Search";
-import LoginRegister from "./pages/LoginRegister";
+import { Route, Routes } from 'react-router';
+import Main from './pages/Main';
+import NotFound from './pages/NotFound';
+import './styles/reset.css';
+import Search from './pages/Search';
+import LoginRegister from './pages/LoginRegister';
+import MyPage from './pages/MyPage';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<LoginRegister />} />
+        <Route path="/mypage" element={<MyPage />} />
         <Route path="/artDetail/:artId" element={<Main />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/search" element={<Search></Search>}></Route>
