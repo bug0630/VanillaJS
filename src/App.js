819830e2e48a62e266974da034533a4ed4fc676d
UrtnasanT <urtnasan.togtokhsuren@gmail.com>
2024-05-03 15:37:39 +0900

    style: auction page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import Main from "./pages/Main";
 import NotFound from "./pages/NotFound";
 import "./styles/reset.css";
+import Auction from "./pages/Auction";
 
 function App() {
   return (
@@ -11,8 +12,10 @@ function App() {
         <Route path="/login" element={<Main />} />
         <Route path="/artDetail/:artId" element={<Main />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="auction" element={<Auction />} />
+        
       </Routes>
-      <LoginRegister />
+      
     </div>
   );
 }
