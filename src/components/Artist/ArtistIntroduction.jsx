5dbdb20549ab359d17b027a299a17794c49ea2f2
Aningoo <aningooaningoo@Aningooui-noteubug.local>
2024-05-03 22:56:19 +0900

    init: artist page 구성

--- src/components/Artist/ArtistIntroduction.jsx
+++ src/components/Artist/ArtistIntroduction.jsx
@@ -7,22 +7,32 @@ const ArtistIntroduction = () => {
   const [artistData, setArtistData] = useState(jsonData);
 
   return (
-    <div >
+    <div className='artist-introduction'>
       {artistData && (
-        <div className='art-style'>
-          <h1>{artistData[0].artist_name}</h1>
-          <p>{artistData[0].description}</p>
-          <img src={artistData[0].profile_img} alt="Artist Profile" />
-          <ul>
+        <div className='artist-info'>
+          <div className='profile'>
+          <img src={`${process.env.PUBLIC_URL}/images/artist_img/${artistData[0].profile_img}`} alt="Artist Profile" 
+          style={{ width: '180px', height: '200px'}}/>
+          </div>
+
+          <div className='details'>
+            <h1>{artistData[0].artist_name}</h1>
+            <p>Birthdate: {artistData[0].birthdate}</p>
+            <p>{artistData[0].description}</p>
+            <p>Email: {artistData[0].email}</p>
+            <p>Instagram: <a href={artistData[0].insta_link} target="_blank" rel="noopener noreferrer">{artistData[0].artist_name}</a></p>
+          </div>
+          <div className='artworks'>
+            <h2>Artworks</h2>
+            <ul>
             {artistData[0].art.map(artwork => (
               <li key={artwork.id}>
                 <img src={`/images/art_img/${artwork.art_img}`} alt={artwork.title} />
                 <p>{artwork.title}</p>
               </li>
             ))}
-          </ul>
-          <p>Email: {artistData[0].email}</p>
-          <p>Instagram: <a href={artistData[0].insta_link} target="_blank" rel="noopener noreferrer">{artistData[0].artist_name}</a></p>
+            </ul>
+          </div>
         </div>
       )}
     </div>
