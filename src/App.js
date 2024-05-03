0451e62ba87607ba6af2f6cf2e017641bbdb4edd
zaya <zayadreambig@gmail.com>
2024-05-03 14:55:35 +0900

    style: login 및 register page style

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Main from "./pages/Main";
 import NotFound from "./pages/NotFound";
 import "./styles/reset.css";
 import Search from "./pages/Search";
+import LoginRegister from "./pages/LoginRegister";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Main />} />
+        <Route path="/login" element={<LoginRegister />} />
         <Route path="/artDetail/:artId" element={<Main />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/search" element={<Search></Search>}></Route>
