ef1f491192ee1dbb9ac43d04d757d8a83a4fcea8
Aningoo <aningooaningoo@Aningooui-noteubug.local>
2024-05-04 22:59:55 +0900

    fix: ArtistInfo

--- src/components/Artist/ArtistIntroduction.jsx
+++ src/components/Artist/ArtistIntroduction.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import jsonData from '../../data/artist.json'
 import '../../styles/ArtistIntroduction.css'
+import { FaMailBulk, FaInstagram } from "react-icons/fa";
 
 
 const ArtistIntroduction = () => {
@@ -12,19 +13,24 @@ const ArtistIntroduction = () => {
         <div className='artist-info'>
           <div className='profile'>
           <img src={`${process.env.PUBLIC_URL}/images/artist_img/${artistData[0].profile_img}`} alt="Artist Profile" 
-          style={{ width: '180px', height: '200px'}}/>
+          style={{ width: '300px', height: '350px', borderRadius: '15px'}}/>
           </div>
 
           <div className='details'>
-            <h1>{artistData[0].artist_name}</h1>
+            <h1 className='artist-name'>{artistData[0].artist_name}</h1>
             <p>{artistData[0].birthdate}</p>
             <br />
             <p>{artistData[0].description}</p>
             <div className='details-right'>
-              <p>Email: {artistData[0].email}</p>
-              <p>Instagram: <a href={artistData[0].insta_link} 
-              target="_blank" rel="noopener noreferrer">
-              {artistData[0].artist_name}</a></p>
+              <div className='Email'>
+                <FaMailBulk className="icon" />
+                <p>: {artistData[0].email}</p></div>
+              <div className='instagram-link'>
+                <FaInstagram className="icon" />
+                <p>: <a href={artistData[0].insta_link} 
+                target="_blank" rel="noopener noreferrer">
+                  {artistData[0].artist_name}</a></p>
+              </div>
             </div>
           </div>
           <div className='artworks'>
