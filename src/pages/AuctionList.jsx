7639ba17632b54857a7ca3080880b045947c9e9d
SOM-R2 <sjleee406@naver.com>
2024-05-11 15:42:34 +0900

    style : 클래스 명 수정

--- src/pages/AuctionList.jsx
+++ src/pages/AuctionList.jsx
@@ -2,6 +2,7 @@ import AuctionCountdown from '../components/Auction/AuctionCountdown';
 import AuctionItemList from '../components/Auction/AuctionItemList';
 import auctionData from '../data/auction.json';
 import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
 import '../styles/AuctionList.scss';
 
 export default function AuctionList() {
@@ -22,18 +23,22 @@ export default function AuctionList() {
   ];
   const formattedDate = `${months[today.getMonth()]} ${today.getDate()}`;
   return (
-    <div className="auctionList">
-      <div className="title">
-        <h3>today's auction</h3>
-        <span>{formattedDate}(10AM)</span>
-        <span> Open</span>
-        <AuctionCountdown />
+    <>
+      <Header />
+      <div className="auction-List">
+        <div className="auction-title">
+          <h3>today's auction</h3>
+          <span>{formattedDate}(10AM)</span>
+          <span> Open</span>
+          <AuctionCountdown />
+        </div>
+        <div>오늘의 특별한 작품들을 만나보세요.</div>
+        <AuctionItemList data={auctionData} type="bid" />
+        <div className="auction-button">
+          <button>Place your bid</button>
+        </div>
       </div>
-      <div>오늘의 특별한 작품들을 만나보세요.</div>
-      <AuctionItemList data={auctionData} type="bid" />
-      <div className="auction-button">
-        <button>Place your bid</button>
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
