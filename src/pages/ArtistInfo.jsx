4bbd5619be1de371d1daaa573469fa3781da26aa
Aningoo <aningooaningoo@Aningooui-noteubug.local>
2024-05-11 15:02:16 +0900

    fix: artistinfo

--- src/pages/ArtistInfo.jsx
+++ src/pages/ArtistInfo.jsx
@@ -1,8 +1,10 @@
-
+import Header from '../components/header/Header'
 import ArtistBanner from '../components/Artist/ArtistBanner'
 import ArtistDetails from '../components/Artist/ArtistDetails';
 import Artwork from '../components/Artist/Artwork';
 import Recommend from '../components/Artist/Recommend';
+import Footer from '../components/footer/Footer'
+
 
 
 export default function Artist() {
@@ -12,12 +14,13 @@ export default function Artist() {
      
   return (
     <div>
-      
-       <ArtistBanner h1Text="아티스트 소개" /> 
-       {modifiedH1({ children: "" })}
-       <ArtistDetails/>
-       <Artwork/>
-       <Recommend/>
+      <Header />
+      <ArtistBanner h1Text="아티스트 소개" /> 
+      {modifiedH1({ children: "" })}
+      <ArtistDetails/>
+      <Artwork/>
+      <Recommend/>
+      <Footer />
     </div>
   );
 }
