be81dc50a83fe70b5d662ec5d5245011bc0c0030
Aningoo <aningooaningoo@Aningooui-noteubug.local>
2024-05-15 00:17:51 +0900

    fix: artDetail 페이지 ->  artistInfo 수정

--- src/components/Art/ArtistInfo.jsx
+++ src/components/Art/ArtistInfo.jsx
@@ -13,41 +13,38 @@ const ArtistInfo = (props) => {
   useEffect(() => {
     const productData = productsData.find(product => product.id === parseInt(artistId));
     setProduct(productData);
-    setImages(productData.profile_img) // 변경된 부분
-    setActiveImage(productData.profile_img[0]) // 변경된 부분
+    setImages(productData.profile_img) 
+    setActiveImage(productData.profile_img[0]) 
   }, [artistId]);
 
   
   return (
-    <div className="product_detail">
-     
-        <img src={activeImg} alt="" className="artistInfo_img" />
-          {/* 여기서 art_img를 profile_img로 변경 */}
-          <img src={images[0]} alt="" className="profile_img" onClick={() => setActiveImage(images[0])} />
-      <div>
+    <div className="artist_detail">
+      
       <div className="infoItem">
-            <div className="infoValue">{product && product.artist_name}</div>
+        <div className="infoLabel">작품 소개</div>
+        <div className="infoValue">{product && product.description}</div>
+      </div>
+     
+      <img src={product && product.profile_img} alt="" className="artistInfo_img" />
+      
+        <div>
+          <div className="infoItem">
+            <div className="artistInfo_name">{product && product.artist_name}</div>
           </div>
-
-          
-
+            
           <div className='Email'>
             <p><FaMailBulk className="iconEmail" />{product && product.email}</p>
           </div>
 
           <div className='instagram-link'>
-                    <p> <FaInstagram className="icon" />
-                    <a
-                    href={product && product.insta_link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >Instagram
-                    </a>
-                    </p>
-                   </div>
-      </div>
+             <p> <FaInstagram className="icon" />
+            <a href={product && product.insta_link} target="_blank" rel="noopener noreferrer"
+            >Instagram</a></p>
+          </div>
 
-    </div>
+        </div>
+      </div>
   );
 };
 
