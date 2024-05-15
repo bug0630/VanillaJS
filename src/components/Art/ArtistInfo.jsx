4f64c513c48d6dd7acadee4b964c78dcadd9e373
zaya <zayadreambig@gmail.com>
2024-05-15 15:11:53 +0900

    ArtDetail page artist image

--- src/components/Art/ArtistInfo.jsx
+++ src/components/Art/ArtistInfo.jsx
@@ -1,52 +1,65 @@
-import React, { useState, useEffect } from "react";
-import '../../styles/ArtDetail.css'
-import productsData from "../../data/art.json";
+import React, { useState, useEffect } from 'react';
+import '../../styles/ArtDetail.css';
+import productsData from '../../data/art.json';
 import { FaMailBulk, FaInstagram } from 'react-icons/fa';
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 const ArtistInfo = (props) => {
   const { artistId } = useParams();
   const [product, setProduct] = useState(null);
-  const [images, setImages] = useState([])
+  const [images, setImages] = useState([]);
   const [activeImg, setActiveImage] = useState('');
 
   useEffect(() => {
-    const productData = productsData.find(product => product.id === parseInt(artistId));
+    const productData = productsData.find(
+      (product) => product.id === parseInt(artistId)
+    );
     setProduct(productData);
-    setImages(productData.profile_img) 
-    setActiveImage(productData.profile_img[0]) 
+    setImages(productData.profile_img);
+    setActiveImage(productData.profile_img[0]);
   }, [artistId]);
 
-  
   return (
     <div className="artist_detail">
-      
       <div className="infoItem">
         <div className="infoLabel">작품 소개</div>
         <div className="infoValue">{product && product.description}</div>
       </div>
-     
-      <img src={product && product.profile_img} alt="" className="artistInfo_img" />
-      
-        <div>
-          <div className="infoItem">
-            <div className="artistInfo_name">{product && product.artist_name}</div>
+      <img
+        src={product && product.profile_img}
+        alt=""
+        className="artistInfo_img"
+      />
+      <div className="artistInfo">
+        <div className="infoItem">
+          <div className="artistInfo_name">
+            {product && product.artist_name}
           </div>
-            
-          <div className='Email'>
-            <p><FaMailBulk className="iconEmail" />{product && product.email}</p>
-          </div>
+        </div>
 
-          <div className='instagram-link'>
-             <p> <FaInstagram className="icon" />
-            <a href={product && product.insta_link} target="_blank" rel="noopener noreferrer"
-            >Instagram</a></p>
-          </div>
+        <div className="Email">
+          <p>
+            <FaMailBulk className="iconEmail" />
+            {product && product.email}
+          </p>
+        </div>
 
+        <div className="instagram-link">
+          <p>
+            {' '}
+            <FaInstagram className="icon" />
+            <a
+              href={product && product.insta_link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+          </p>
         </div>
       </div>
+    </div>
   );
 };
 
 export default ArtistInfo;
-
