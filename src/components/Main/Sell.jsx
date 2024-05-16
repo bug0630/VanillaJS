8082af0104fa4c5252fa7cf74c79dedfa939fdc8
bug0630 <bugbdh@gmail.com>
2024-05-16 16:35:38 +0900

    fix:img

--- src/components/Main/Sell.jsx
+++ src/components/Main/Sell.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
+import { useEffect, useState } from 'react';
+
 export default function Sell() {
+  const [showBoardSection, setShowBoardSection] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 3200 && !showBoardSection) {
+        setShowBoardSection(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [showBoardSection]); // showBoardSection 상태가 변경될 때마다 useEffect 재실행
+
   return (
-    <div className="sellWrap">
+    <div className={`sellWrap ${showBoardSection ? 'show' : ''}`}>
       <h2>Sell width C'Arte</h2>
       <div className="sellbox">
         <div className="logoSection">
