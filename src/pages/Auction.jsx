9c046b31cba4cf35bf83b17909643ae7519f4162
zaya <zayadreambig@gmail.com>
2024-05-16 23:23:16 +0900

    Curtain error

--- src/pages/Auction.jsx
+++ src/pages/Auction.jsx
@@ -8,26 +8,26 @@ import Footer from '../components/footer/Footer';
 import AuctionCountdown from '../components/Auction/AuctionCountdown';
 
 const AuctionPage = () => {
-   // 커튼 요소를 참조하기 위한 ref 생성
-   const curtainRef = useRef(null);
+  // 커튼 요소를 참조하기 위한 ref 생성
+  const curtainRef = useRef(null);
 
-   // 커튼 애니메이션이 종료되면 커튼을 숨기는 함수
-   const hideCurtain = () => {
-     // 커튼 요소가 존재하고 애니메이션이 끝나면
-     if (curtainRef.current) {
-       // 커튼 숨기기
-       curtainRef.current.style.display = 'none';
-     }
-   };
- 
-   // 컴포넌트가 마운트된 후 애니메이션이 종료되면 커튼을 숨기도록 설정
-   useEffect(() => {
-     // 애니메이션 지속 시간 (8초) 후에 커튼 숨기기 함수 호출
-     const timeoutId = setTimeout(hideCurtain, 5000); // 8초는 애니메이션 지속 시간과 동일해야 합니다.
- 
-     // 컴포넌트 언마운트 시 타임아웃 클리어
-     return () => clearTimeout(timeoutId);
-   }, []); // 컴포넌트가 처음 렌더링될 때만 호출
+  // 커튼 애니메이션이 종료되면 커튼을 숨기는 함수
+  const hideCurtain = () => {
+    // 커튼 요소가 존재하고 애니메이션이 끝나면
+    if (curtainRef.current) {
+      // 커튼 숨기기
+      curtainRef.current.style.display = 'none';
+    }
+  };
+
+  // 컴포넌트가 마운트된 후 애니메이션이 종료되면 커튼을 숨기도록 설정
+  useEffect(() => {
+    // 애니메이션 지속 시간 (8초) 후에 커튼 숨기기 함수 호출
+    const timeoutId = setTimeout(hideCurtain, 5000); // 8초는 애니메이션 지속 시간과 동일해야 합니다.
+
+    // 컴포넌트 언마운트 시 타임아웃 클리어
+    return () => clearTimeout(timeoutId);
+  }, []); // 컴포넌트가 처음 렌더링될 때만 호출
   const [showModal, setShowModal] = useState(false);
   const [showBidSubmittedModal, setShowBidSubmittedModal] = useState(false);
   const [bidDetails, setBidDetails] = useState({
@@ -36,10 +36,9 @@ const AuctionPage = () => {
     email: '',
     phone: '',
   });
-  const [curtainOpen, setCurtainOpen] = useState(false); // State for curtain
- const [selectedArt, setSelectedArt] = useState(null);
+  const [selectedArt, setSelectedArt] = useState(null);
   const handleSelectArt = (artId) => {
-    const auction = artData.find((item) => item.id === artId); 
+    const auction = artData.find((item) => item.id === artId);
     setSelectedArt(auction);
   };
   useEffect(() => {
@@ -55,8 +54,8 @@ const AuctionPage = () => {
   const handleShowBidSubmittedModal = () => setShowBidSubmittedModal(true);
 
   const handleBidSubmit = () => {
-    handleCloseModal(); 
-    handleShowBidSubmittedModal(); 
+    handleCloseModal();
+    handleShowBidSubmittedModal();
     // You can perform additional actions here, such as sending bid details to the server
   };
 
@@ -73,158 +72,212 @@ const AuctionPage = () => {
   //     setRemainingTime(calculateRemainingTime(auctionEndTime));
   //   }, 500);
 
-    // Cleanup function to clear the interval when component unmounts
+  // Cleanup function to clear the interval when component unmounts
   return (
     <>
-    <Header/>
-   
-    
-    <div className="auction-page">
-       {/*------ curtain effect-------- */}
-       <div className="outer">
-      <div className="tcell">
-        <div className="curtain-wrapper">
-            <div className="curtain" ref={curtainRef}>
-              <div className="panel-left"></div>
-              <div className="panel-right"></div>
-            <div className="jacket" title="Play"></div>
-          </div>
-        </div>
-      </div>
-     </div>
+      <Header />
+
+      <div className="auction-page">
         {/*------ curtain effect-------- */}
-     {/*------ curtain effect-------- */}
-     {/* <div className="outer">
+        <div className="outer">
           <div className="tcell">
             <div className="curtain-wrapper">
-              <div className={`curtain ${curtainOpen ? 'open' : ''}`}>
+              <div className="curtain" ref={curtainRef}>
                 <div className="panel-left"></div>
                 <div className="panel-right"></div>
-                <div className="jacket" title="Play" onClick={toggleCurtain}></div>
+                <div className="jacket" title="Play"></div>
               </div>
             </div>
           </div>
-        </div> */}
-        {/*------ curtain effect-------- */}
-       
-       
-        <div className="artwork-container">
-  {selectedArt && (
-    <>
-    <div className="artwork-img-section">
-      <img src={selectedArt.bid_art_img ? selectedArt.bid_art_img : ''} className="artwork-image" />
-    </div>
-      <div className="artwork-info"> 
-        <h2 className="artwork-title">{selectedArt.bid_art_title}</h2>
-        <h3 className="artist">Artist: {selectedArt.artist}</h3>
-        <p className="description">Description: {selectedArt.description}</p>
-        
-        <div className="price-section">
-          <div>Estimated Price:
-          <span className="price-value">${selectedArt.bid_estimate_price}</span>
-          </div> 
-          <div>Starting Price:
-          <span className="price-value">${selectedArt.bid_starting_price}</span>
-          </div>
         </div>
-        
-        <div key={selectedArt.id}>
-          <p>Published Year: {selectedArt.published_year}</p>
-          <p> Bid Code:{selectedArt.bid_code}</p>
-          <p>Date: {selectedArt.bid_date}</p>
-          
-          {/* <div className="bid-section">
+
+        <div className="artwork-container">
+          {selectedArt && (
+            <>
+              <div className="artwork-img-section">
+                <img
+                  src={selectedArt.bid_art_img ? selectedArt.bid_art_img : ''}
+                  className="artwork-image"
+                />
+              </div>
+              <div className="artwork-info">
+                <h2 className="artwork-title">{selectedArt.bid_art_title}</h2>
+                <h3 className="artist">Artist: {selectedArt.artist}</h3>
+                <p className="description">
+                  Description: {selectedArt.description}
+                </p>
+
+                <div className="price-section">
+                  <div>
+                    Estimated Price:
+                    <span className="price-value">
+                      ${selectedArt.bid_estimate_price}
+                    </span>
+                  </div>
+                  <div>
+                    Starting Price:
+                    <span className="price-value">
+                      ${selectedArt.bid_starting_price}
+                    </span>
+                  </div>
+                </div>
+
+                <div key={selectedArt.id}>
+                  <p>Published Year: {selectedArt.published_year}</p>
+                  <p> Bid Code:{selectedArt.bid_code}</p>
+                  <p>Date: {selectedArt.bid_date}</p>
+
+                  {/* <div className="bid-section">
             <p className="bid-label">Price: {selectedArt.price}</p>
           </div>  */}
-        </div>
-        
-        {/* <div className="timer-container">
+                </div>
+
+                {/* <div className="timer-container">
           <div className="timer-frame"> 
             <p className="remaining-time">Closing:{formatRemainingTime(remainingTime)}</p>
           </div>
         </div> */}
-        <AuctionCountdown/>
-        {bidDetails.amount && (
-          <div className="bid-details">
-            <h4>Your Bid Details:</h4>
-            <p><strong>Amount:</strong> ${bidDetails.amount}</p>
-            <p><strong>Name:</strong> {bidDetails.name}</p>
-          </div>
-        )}
-          <button className="bid-button" onClick={handleShowModal}>Place your bid</button>
-      
-      </div>  
-    </>
-  )}
-</div>
-
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Place Your Bid</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <form>
-            <div className="mb-3">
-              <label htmlFor="bidAmount" className="form-label">경매 가격</label>
-              <div className="input-group">
-                <span className="input-group-text">₩</span>
-                <input type="text" className="form-control" id="bidAmount" value={bidDetails.amount} onChange={(e) => setBidDetails({...bidDetails, amount: e.target.value})} />
+                <AuctionCountdown />
+                {bidDetails.amount && (
+                  <div className="bid-details">
+                    <h4>Your Bid Details:</h4>
+                    <p>
+                      <strong>Amount:</strong> ${bidDetails.amount}
+                    </p>
+                    <p>
+                      <strong>Name:</strong> {bidDetails.name}
+                    </p>
+                  </div>
+                )}
+                <button className="bid-button" onClick={handleShowModal}>
+                  Place your bid
+                </button>
               </div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="bidderName" className="form-label">성함</label>
-              <input type="text" className="form-control" id="bidderName" value={bidDetails.name} onChange={(e) => setBidDetails({...bidDetails, name: e.target.value})} />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="bidderEmail" className="form-label">이메일</label>
-              <input type="email" className="form-control" id="bidderEmail" value={bidDetails.email} onChange={(e) => setBidDetails({...bidDetails, email: e.target.value})} />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="bidderPhone" className="form-label">전화번호</label>
-              <input type="tel" className="form-control" id="bidderPhone" value={bidDetails.phone} onChange={(e) => setBidDetails({...bidDetails, phone: e.target.value})} />
-            </div>
-          </form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleBidSubmit}>
-            Submit Bid
-          </Button>
-        </Modal.Footer>
-      </Modal>
+            </>
+          )}
+        </div>
 
-      <Modal show={showBidSubmittedModal} onHide={handleCloseBidSubmittedModal}>
-  <Modal.Header closeButton>
-    <Modal.Title>Bid Submitted</Modal.Title>
-  </Modal.Header>
-  <Modal.Body>
-    <div style={{ textAlign: 'center' }}>
-      <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/successful-bid-3-613041.png" alt="Success Icon" style={{ width: '100px', marginBottom: '20px' }} />
-      <h4>Congratulations!</h4>
-      <p>입찰서가 성공적으로 제출되었습니다!</p>
-    </div>
-    {bidDetails.amount && (
-    <div className="bid-details">
-      <h4>Your Bid Details:</h4>
-      <p><strong>Amount:</strong> ${bidDetails.amount}</p>
-      <p><strong>Name:</strong> {bidDetails.name}</p>
-      <p><strong>Email:</strong> {bidDetails.email}</p>
-      <p><strong>Phone:</strong> {bidDetails.phone}</p>
-    </div>
-  )}
-  </Modal.Body>
-  <Modal.Footer>
-    <Button variant="primary" onClick={handleCloseBidSubmittedModal}>
-     닫기
-    </Button>
-  </Modal.Footer>
-</Modal>
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Place Your Bid</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form>
+              <div className="mb-3">
+                <label htmlFor="bidAmount" className="form-label">
+                  경매 가격
+                </label>
+                <div className="input-group">
+                  <span className="input-group-text">₩</span>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="bidAmount"
+                    value={bidDetails.amount}
+                    onChange={(e) =>
+                      setBidDetails({ ...bidDetails, amount: e.target.value })
+                    }
+                  />
+                </div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="bidderName" className="form-label">
+                  성함
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="bidderName"
+                  value={bidDetails.name}
+                  onChange={(e) =>
+                    setBidDetails({ ...bidDetails, name: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="bidderEmail" className="form-label">
+                  이메일
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="bidderEmail"
+                  value={bidDetails.email}
+                  onChange={(e) =>
+                    setBidDetails({ ...bidDetails, email: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="bidderPhone" className="form-label">
+                  전화번호
+                </label>
+                <input
+                  type="tel"
+                  className="form-control"
+                  id="bidderPhone"
+                  value={bidDetails.phone}
+                  onChange={(e) =>
+                    setBidDetails({ ...bidDetails, phone: e.target.value })
+                  }
+                />
+              </div>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleBidSubmit}>
+              Submit Bid
+            </Button>
+          </Modal.Footer>
+        </Modal>
 
-</div>
-<Footer/>
-</>
+        <Modal
+          show={showBidSubmittedModal}
+          onHide={handleCloseBidSubmittedModal}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Bid Submitted</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div style={{ textAlign: 'center' }}>
+              <img
+                src="https://cdn.iconscout.com/icon/premium/png-256-thumb/successful-bid-3-613041.png"
+                alt="Success Icon"
+                style={{ width: '100px', marginBottom: '20px' }}
+              />
+              <h4>Congratulations!</h4>
+              <p>입찰서가 성공적으로 제출되었습니다!</p>
+            </div>
+            {bidDetails.amount && (
+              <div className="bid-details">
+                <h4>Your Bid Details:</h4>
+                <p>
+                  <strong>Amount:</strong> ${bidDetails.amount}
+                </p>
+                <p>
+                  <strong>Name:</strong> {bidDetails.name}
+                </p>
+                <p>
+                  <strong>Email:</strong> {bidDetails.email}
+                </p>
+                <p>
+                  <strong>Phone:</strong> {bidDetails.phone}
+                </p>
+              </div>
+            )}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleCloseBidSubmittedModal}>
+              닫기
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+      <Footer />
+    </>
   );
 };
 
